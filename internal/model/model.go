d106d7ddcf68aa513aa9f88aba157beb916bfa10
ViharevN <swix_off@mail.ru>
2024-04-14 14:45:19 +0300

    refactoring code, add errors in api

--- internal/model/model.go
+++ internal/model/model.go
@@ -18,7 +18,6 @@ type RoomAvailability struct {
 	HotelID string    `json:"hotel_id"`
 	RoomID  string    `json:"room_id"`
 	Date    time.Time `json:"date"`
-	Quota   int       `json:"quota"`
 }
 
 type Room struct {
